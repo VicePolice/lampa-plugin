cfa2b66ebb9c4a9879e7881947d52c77c88bed26
Hlushok <101033368+Hlushok@users.noreply.github.com>
2026-02-19 21:19:18 +0200

    Refactor Uakino plugin and add UakinoParser

--- test.js
+++ test.js
@@ -1,176 +1,136 @@
 (function () {
     'use strict';
 
-    Lampa.Platform.tv();
-
     /**
-     * Плагін для Uakino.best (Lampac)
-     * Базується на логіці CloudStream UakinoProvider
+     * Парсер Uakino для Lampa/Lampac
      */
-    function Uakino(object) {
+    function UakinoParser(object) {
         var network = new Lampa.Reguest();
-        var scroll  = new Lampa.Scroll({mask: true, over: true});
-        var items   = [];
-        var extract = {};
         var mainUrl = "https://uakino.best";
-
-        // Селектори та регулярки з вашого коду
+        
+        // Ті самі регулярки з вашого Kotlin-коду
         const fileRegex = /file\s*:\s*["']([^",']+?)["']/;
-        const subsRegex = /subtitle\s*:\s*["']([^",']+?)["']/;
+        
+        this.search = function (data) {
+            // Lampa передає об'єкт фільму (title, year)
+            var query = data.movie.title;
+            var searchUrl = mainUrl;
 
-        this.create = function () {
-            this.activity.loader(true);
-            this.start();
-            return this.render();
+            network.silent(searchUrl, (html) => {
+                this.parseSearch(html, data);
+            }, object.error, {
+                do: 'search',
+                subaction: 'search',
+                story: query.replace(/ /g, '+')
+            }, { method: 'POST' });
         };
 
-        this.start = function () {
-            var url = object.url;
-            if (object.search) {
-                // Логіка пошуку (POST запит)
-                network.silent(mainUrl, (html) => {
-                    this.build(html);
-                }, (e) => {
-                    this.empty();
-                }, {
-                    do: 'search',
-                    subaction: 'search',
-                    story: object.search.replace(/ /g, '+')
-                }, {method: 'POST'});
-            } else {
-                // Логіка категорій
-                network.silent(url, (html) => {
-                    this.build(html);
-                }, this.empty.bind(this));
-            }
-        };
-
-        this.build = function (html) {
+        this.parseSearch = function (html, data) {
             var $ = cheerio.load(html);
-            var container = $('.owl-item, .movie-item');
+            var results = [];
+            var items = $('.movie-item');
 
-            container.each((i, el) => {
+            items.each((i, el) => {
                 var a = $(el).find('a.movie-title, a.full-movie');
+                var title = a.text().trim();
                 var href = a.attr('href');
                 
-                // Фільтр "чорних" посилань (news/franchise)
-                if (href && !href.match(/(\/news\/)|(\/franchise\/)/)) {
-                    var title = a.text().trim() || $(el).find('.full-movie-title').text().trim();
-                    var img = $(el).find('img').attr('src');
-                    
-                    items.push({
+                // Перевірка на відповідність року (щоб не було зайвого)
+                // Можна додати фільтрацію за назвою тут
+                if (href) {
+                    results.push({
                         title: title,
-                        url: href,
-                        img: img ? (img.startsWith('http') ? img : mainUrl + img) : '',
-                        method: href.includes('/seriesss/') || href.includes('/anime-series/') ? 'serial' : 'movie'
+                        url: href
                     });
                 }
             });
 
-            if (items.length) {
-                this.display();
+            if (results.length > 0) {
+                // Вибираємо перший найбільш схожий результат
+                this.loadMovie(results[0].url, data);
             } else {
-                this.empty();
+                object.error();
             }
         };
 
-        this.display = function () {
-            this.activity.loader(false);
-            // Тут логіка рендеру карток у Lampa
-            items.forEach(item => {
-                var card = Lampa.Template.get('card', item);
-                card.on('click', () => {
-                    this.loadDetails(item);
-                });
-                scroll.append(card);
-            });
-        };
-
-        // Отримання посилань (LoadLinks логіка)
-        this.loadDetails = function (item) {
-            Lampa.Select.show({
-                title: 'Вибір якості/озвучки',
-                items: [{title: 'Завантаження...', info: ''}],
-                onSelect: (sel) => {}
-            });
-
-            network.silent(item.url, (html) => {
+        this.loadMovie = function (url, data) {
+            network.silent(url, (html) => {
                 var $ = cheerio.load(html);
-                var id = item.url.split('/').pop().split('-')[0];
-                var iframeUrl = $('#pre').attr('src') || $('#pre').attr('data-src');
+                var id = url.split('/').pop().split('-')[0];
+                var isSerial = url.includes('/seriesss/') || url.includes('/anime-series/');
 
-                if (item.method === 'serial') {
-                    // AJAX для серіалів
+                if (isSerial) {
+                    // Логіка для серіалів через AJAX
                     var ajaxUrl = `${mainUrl}/engine/ajax/playlists.php?news_id=${id}&xfield=playlist&time=${Date.now()}`;
-                    network.silent(ajaxUrl, (data) => {
-                        if (data.success) {
-                            var $s = cheerio.load(data.response);
-                            var episodes = [];
+                    network.silent(ajaxUrl, (res) => {
+                        if (res.success) {
+                            var $s = cheerio.load(res.response);
+                            var files = [];
                             $s('.playlists-videos li').each((i, el) => {
-                                episodes.push({
+                                files.push({
                                     title: $(el).text().trim(),
                                     file: $(el).attr('data-file'),
-                                    voice: $(el).attr('data-voice')
+                                    quality: $(el).attr('data-voice') || 'UA'
                                 });
                             });
-                            this.showEpisodes(episodes);
+                            this.success(files, data);
                         }
-                    }, null, null, {
+                    }, object.error, null, {
                         headers: { 'X-Requested-With': 'XMLHttpRequest', 'Referer': mainUrl }
                     });
                 } else {
-                    // Пряме витягування для фільму
-                    if (iframeUrl) this.extractVideo(iframeUrl);
+                    // Логіка для фільмів
+                    var iframeUrl = $('#pre').attr('src') || $('#pre').attr('data-src');
+                    if (iframeUrl) {
+                        this.extractDirect(iframeUrl, data);
+                    } else {
+                        object.error();
+                    }
                 }
             });
         };
 
-        this.extractVideo = function (url) {
-            if (!url.startsWith('http')) url = 'https:' + url;
-            network.silent(url, (scriptHtml) => {
-                var fileMatch = scriptHtml.match(fileRegex);
-                if (fileMatch) {
-                    var videoUrl = fileMatch[1];
-                    Lampa.Player.play({
-                        url: videoUrl,
-                        title: object.title
-                    });
+        this.extractDirect = function (iframeUrl, data) {
+            if (!iframeUrl.startsWith('http')) iframeUrl = 'https:' + iframeUrl;
+            network.silent(iframeUrl, (scriptHtml) => {
+                var match = scriptHtml.match(fileRegex);
+                if (match) {
+                    var files = [{
+                        title: 'Uakino Video',
+                        file: match[1],
+                        quality: '720p/1080p'
+                    }];
+                    this.success(files, data);
                 }
-            }, null, null, { headers: { 'Referer': mainUrl } });
+            }, object.error, null, { headers: { 'Referer': mainUrl } });
         };
 
-        this.empty = function () {
-            this.activity.loader(false);
-            // Відобразити "порожньо"
-        };
-
-        this.render = function () {
-            return scroll.render();
+        this.success = function (files, data) {
+            var result = files.map(f => {
+                return {
+                    title: f.title,
+                    url: f.file.startsWith('http') ? f.file : 'https:' + f.file,
+                    quality: f.quality
+                };
+            });
+            object.success(result);
         };
     }
 
-    // Реєстрація плагіна в системі
-    Lampa.Component.add('uakino', Uakino);
-
+    // Реєстрація як джерела відео
     function startPlugin() {
-        window.uakino_plugin = true;
-        Lampa.Menu.add({
-            id: 'uakino',
-            title: 'Uakino',
-            icon: `<svg...>`, // тут можна додати іконку
-            onSelect: function () {
-                Lampa.Activity.push({
-                    url: 'https://uakino.best/filmy/',
-                    title: 'Uakino',
-                    component: 'uakino',
-                    page: 1
-                });
-            }
+        Lampa.Component.add('uakino_parser', UakinoParser);
+        
+        // Додаємо кнопку "Uakino" в меню парсерів
+        Lampa.Metrika.cache('uakino_parser', 10); // кешуємо результати на 10 хв
+        
+        Lampa.Player.onSearch((data) => {
+            return new UakinoParser(data);
         });
     }
 
     if (window.appready) startPlugin();
-    else Lampa.Listener.follow('app', function (e) {
+    else Lampa.Listener.follow('app', (e) => {
         if (e.type == 'ready') startPlugin();
     });
 
